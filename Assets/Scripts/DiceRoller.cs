d04fd2949969e43b8400ebbb5aaaf00c3939090f
Jan van der Zijden <jan.van.der.zijden@gmail.com>
2022-07-16 18:19:55 +0200

    Added extra combos
    Added full house and tiny to huge street.

--- Assets/Scripts/DiceRoller.cs
+++ Assets/Scripts/DiceRoller.cs
@@ -56,16 +56,87 @@ public class DiceRoller : MonoBehaviour
         List<int> numbers = new List<int>();
         foreach (var dice in dices) numbers.Add(dice.GetNumber());
 
-        
-        for (int i = 1; i < 6; i++)
+        List<int> sameNumbersList = new List<int>();
+
+        for (int i = 1; i <= 6; i++)
         {
-            int sameNumbers = numbers.FindAll(x => x == i).Count;
+            int sameNumbers = numbers.FindAll(x => x.Equals(i)).Count;
+            sameNumbersList.Add(sameNumbers);
             //Debug.Log("for " + i + "there is " + sameNumbers + " times the same number");
-            if (sameNumbers > 2)
+
+            if (sameNumbers == 3)
             {
-                Debug.Log("You have trown a three of a kind!");
+                Debug.Log("You threw a three of a kind!");
+            }
+            else if (sameNumbers == 4)
+            {
+                Debug.Log("You threw a carre!");
+            }
+            else if (sameNumbers == 5)
+            {
+                Debug.Log("You threw a Yathzee!");
+            }
+            else if (sameNumbers >= 6)
+            {
+                Debug.Log("You threw a MEGA Yathzee!");
             }
         }
-        
+
+        //section for detecting streets.
+        int streetCount = 0;
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = i; j < 6; j++)
+            {
+                if (sameNumbersList[j] < 1)
+                {
+                    break;
+                }
+                else
+                {
+                    streetCount++;
+                }
+            }
+            if (streetCount == 3)
+            {
+                Debug.Log("You threw a tiny street!");
+            }
+            else if (streetCount == 4)
+            {
+                Debug.Log("You threw a small street!");
+            }
+            else if (streetCount == 5)
+            {
+                Debug.Log("You threw a big street!");
+            }
+            else if (streetCount >= 6)  //street bigger then 6 is not possible (in theory...)
+            {
+                Debug.Log("You threw a huge street!");
+            }
+            streetCount = 0;
+        }
+
+        //This is for detecting full house
+        bool detectedPair = false;
+        bool detectedThreeOfAKind = false;
+        for (int i = 0; i < 6; i++)
+        {
+            if (sameNumbersList[i] >= 3)
+            {
+                if (detectedThreeOfAKind)
+                {
+                    detectedPair = true;
+                }
+                detectedThreeOfAKind = true;
+            }
+            else if (sameNumbersList[i] == 2)
+            {
+                detectedPair = true;
+            }
+        }
+        if (detectedPair && detectedThreeOfAKind)
+        {
+            Debug.Log("You threw a three of a kind!");
+        }
     }
 }
