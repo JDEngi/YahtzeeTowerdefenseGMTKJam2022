0c1b9075b41a266624d2d3a42a9eb3cc55cb42b1
JDEngi <jelledewit.engi@gmail.com>
2022-07-17 15:35:54 +0200

    Implement healthbar for EnemyGoal

--- Assets/Scripts/EnemyGoal.cs
+++ Assets/Scripts/EnemyGoal.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyGoal : MonoBehaviour
 {
-    public float HealthPoints;
+    public float HealthPoints {
+        get { return _healthPoints; }
+        set { _healthPoints = value;
+            healthBar.fillAmount = _healthPoints / startHealth; 
+        }
+    }
+    private float _healthPoints;
+    private float startHealth;
+
+    public Image healthBar;
 
     public static event Action<float> OnHealthChanged;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startHealth = _healthPoints;        
     }
 
     // Update is called once per frame
