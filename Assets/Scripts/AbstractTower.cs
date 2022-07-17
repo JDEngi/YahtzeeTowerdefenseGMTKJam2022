2f4f4c2ba1588326efc272b9638630fb5dd6eacc
Geert van Ieperen <ieperen3039@outlook.com>
2022-07-17 14:11:50 +0200

    Split abstract tower

--- Assets/Scripts/AbstractTower.cs
+++ Assets/Scripts/AbstractTower.cs
@@ -3,23 +3,18 @@ using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class AbstractTower : MonoBehaviour
+public abstract class AbstractTower : MonoBehaviour
 {
-    private GameObject entityContainer;
-    private AbstractEnemy targetEntity;
+    protected GameObject entityContainer;
+
+    [Header("Unity")]
+    public string NameOfEntityContainer;
+    public Sprite Graphic;
 
     [Header("Attributes")]
     public int BuildCost;
     public float Damage;
     public float Range;
-
-    public float fireRate = 1f;
-    private float fireCountdown = 0f;
-
-    [Header("Unity")]
-    public string NameOfEntityContainer;
-    public GameObject bulletPrefab;
-    public Transform firePoint;
 
     // Start is called before the first frame update
     public void Start()
@@ -28,61 +23,37 @@ public class AbstractTower : MonoBehaviour
         if (!entityContainer) throw new Exception("Could not find object with name " + NameOfEntityContainer);
     }
 
+    protected abstract void Shoot();
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, Range);
+    }
+}
+
+public abstract class SingleTargetTower : AbstractTower
+{
+    protected AbstractEnemy targetEntity;
+
     // Update is called once per frame
     public void Update()
     {
-        if (fireCountdown > 0)
+        // if we have a target, check whether it is valid
+        if (targetEntity != null && !targetEntity.IsDestroyed())
         {
-            fireCountdown -= Time.deltaTime;
+            float currentDistance = Vector3.Distance(targetEntity.transform.position, transform.position);
+            if (currentDistance > Range)
+            {
+                targetEntity = null;
+            }
         }
-        else
+
+        // if we have no valid target, search a new one
+        if (targetEntity == null)
         {
-            // if we have a target, check whether it is valid
-            if (targetEntity != null && !targetEntity.IsDestroyed())
-            {
-                float currentDistance = Vector3.Distance(targetEntity.transform.position, transform.position);
-                if (currentDistance > Range)
-                {
-                    targetEntity = null;
-                }
-            }
-
-            // if we have no valid target, search a new one
-            if (targetEntity == null)
-            {
-                targetEntity = SearchEnemyToShoot();
-            }
-
-            // if we have no new target, do nothing
-            if (targetEntity == null)
-            {
-                return;
-            }
-
-            // Shoot
-            if (fireCountdown <= 0f)
-            {
-                Shoot();
-                fireCountdown = 1f / fireRate;
-            }
+            targetEntity = SearchEnemyToShoot();
         }
     }
-
-    private void Shoot()
-    {
-        GameObject bulletGameObject = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Bullet bullet = bulletGameObject.GetComponent<Bullet>();
-
-        if (bullet != null)
-        {
-            bullet.Seek(targetEntity.transform);
-        }
-
-
-        Debug.Log("Shoot!");
-        targetEntity.ApplyDamage(Damage);
-    }
-
     private AbstractEnemy SearchEnemyToShoot()
     {
         AbstractEnemy[] allEnemies = entityContainer.GetComponentsInChildren<AbstractEnemy>();
@@ -93,10 +64,5 @@ public class AbstractTower : MonoBehaviour
         }
 
         return null;
-    }
-
-    public void OnDrawGizmosSelected()
-    {
-        Gizmos.DrawWireSphere(transform.position, Range);
     }
 }
